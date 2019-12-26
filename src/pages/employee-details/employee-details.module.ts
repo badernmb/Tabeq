a145f6e72e21b0b688dde1ea471489e977b1fe5d
Bader Albulayhis <bader@gmail.coml>
2019-12-26 17:35:13 +0300

    test

--- src/pages/employee-details/employee-details.module.ts
+++ src/pages/employee-details/employee-details.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { EmployeeDetailsPage } from './employee-details';
+import { EmployeeDetailsPage } from './employee-details'; 
 
 @NgModule({
   declarations: [
